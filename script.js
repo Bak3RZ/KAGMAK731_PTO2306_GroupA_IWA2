6f49c30a1b6d7b3530b2e242d2ac6c694fd34bcb
Kagiso <101631291+Bak3RZ@users.noreply.github.com>
2023-09-14 10:41:13 +0200

    IWA2
    Challenge 1
    Challenge 2
    Challenge 3

---
+++ script.js
@@ -0,0 +1,37 @@
+//This is the firstname of the user
+
+const user = 'John'
+
+//This is the lastname of the user 
+
+const surname = 'Smith'
+
+console.log('user' + 'surname')
+
+
+/** (c) ACME Inc. 2010 */
+
+/*
+* This is the date that a user created their account 
+*/
+
+const date = '10/07/2014'
+
+console.log(date)
+
+/* 
+* It is important to show the following message in order to:
+* To assure users
+* To scare hackers
+* To impress investors
+*/
+
+const warn = 'Security scan starting'
+
+console.warn(warn)
+
+// It is important to let user know when they can close the page.
+
+const info = 'Please wait for scan to complete before closing the browser.'
+
+console.info(info)
